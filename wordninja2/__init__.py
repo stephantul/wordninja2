c85cc0b29fea2d826c0f010d697251530047d715
stephantul <stephantul@gmail.com>
2022-11-11 08:08:54 +0100

    Add tests, small fixes

---
+++ wordninja2/__init__.py
@@ -0,0 +1,4 @@
+from wordninja2.wordninja2 import WordNinja
+
+
+__all__ = ["WordNinja"]
--- wordninja2/wordninja2.py
+++ wordninja2/wordninja2.py
@@ -9,18 +9,19 @@ WordList = Union[List[str], Dict[str, float]]
 
 
 class WordNinja:
-
     def __init__(self, language: str, wordlist: Optional[List[str]] = None) -> None:
         self.language = language.lower()
         if self.language not in available_languages():
-            raise ValueError(f"{self.language} is not available.")
-        
+            raise ValueError(
+                f"'{self.language}' is not available. Available languages are: {' '.join(set(available_languages()))}"
+            )
+
         if wordlist:
             words = wordlist
         else:
             words = list(get_frequency_dict(self.language))
         costs = np.log(np.arange(1, len(words) + 1) * log(len(words)))
-            
+
         self.words = words
         self.costs = costs
         self.max_cost = max(self.costs) + 1e-3
@@ -36,19 +37,21 @@ class WordNinja:
         out = []
         costs = np.arange(len(string)) * self.max_cost
         lengths = np.ones(len(string), dtype=np.int32)
+        str_len = len(string)
         for end_pos, (cost, length, _) in found:
-            end_pos = min(end_pos + 1, len(string) - 1)
+            # Do not go beyond the end.
+            end_pos = min(end_pos + 1, str_len - 1)
+            # Cost of the current position
             curr_cost = costs[end_pos]
-            actual_cost = cost + costs[end_pos - length]
-            if actual_cost < curr_cost:
+            new_cost = cost + costs[end_pos - length]
+            if new_cost < curr_cost:
                 lengths[end_pos] = length
-                costs[end_pos] = actual_cost
+                costs[end_pos] = new_cost
 
-        i = len(string) - 1 
+        i = str_len - 1
         while i > 0:
             length = lengths[i]
-            out.append(string[i - length: i])
+            out.append(string[i - length : i])
             i -= length
 
         return list(reversed(out))
-
