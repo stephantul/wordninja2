bf18abe3b441adb3a070f3ab890dba739b4075f9
stephantul <stephantul@gmail.com>
2024-08-24 21:14:08 +0200

    Fixed tests

--- wordninja2/__init__.py
+++ wordninja2/__init__.py
@@ -1,3 +1,4 @@
-from wordninja2.wordninja2 import WordNinja
+from wordninja2.utilities import get_default_wordninja
+from wordninja2.wordninja2 import MultiWordNinja, WordNinja
 
-__all__ = ["WordNinja"]
+__all__ = ["WordNinja", "MultiWordNinja", "get_default_wordninja"]
---
+++ wordninja2/utilities.py
@@ -0,0 +1,27 @@
+from gzip import open as gzip_open
+from pathlib import Path
+
+from wordninja2.wordninja2 import WordNinja
+
+_DEFAULT_PATH = Path(__file__).parent / "resources" / "wordninja_words.txt.gz"
+
+
+def _get_default_wordlist() -> list[str]:
+    """
+    Get the default wordlist.
+
+    NOTE: this wordlist was included with the original wordninja.
+
+    :return: The default wordlist.
+    """
+    with gzip_open(_DEFAULT_PATH, "r") as f:
+        return f.read().decode().splitlines()
+
+
+def get_default_wordninja() -> WordNinja:
+    """
+    Get the default WordNinja object.
+
+    :return: A WordNinja object.
+    """
+    return WordNinja(_get_default_wordlist())
